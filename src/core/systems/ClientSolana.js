6e02a86538778e8e4c00b4e60e4aa388e76775d9
Ash <hello@ashconnell.com>
2025-04-08 22:27:05 +1000

    fix: incorrect wallet ref

--- src/core/systems/ClientSolana.js
+++ src/core/systems/ClientSolana.js
@@ -67,9 +67,8 @@ export class ClientSolana extends System {
 
   async onDepositRequest({ depositId, serializedTx }) {
     // console.log('onDepositRequest', { depositId, serializedTx })
-    const player = this.world.entities.player
     const tx = Transaction.from(Buffer.from(serializedTx, 'base64'))
-    const signedTx = await player.wallet.signTransaction(tx)
+    const signedTx = await this.wallet.signTransaction(tx)
     const serializedSignedTx = Buffer.from(signedTx.serialize()).toString('base64')
     this.world.network.send('depositResponse', { depositId, serializedSignedTx })
     // console.log('depositResponse', { depositId, serializedSignedTx })
@@ -77,9 +76,8 @@ export class ClientSolana extends System {
 
   async onWithdrawRequest({ withdrawId, serializedTx }) {
     // console.log('onWithdrawRequest', { withdrawId, serializedTx })
-    const player = this.world.entities.player
     const tx = Transaction.from(Buffer.from(serializedTx, 'base64'))
-    const signedTx = await player.wallet.signTransaction(tx)
+    const signedTx = await this.wallet.signTransaction(tx)
     const serializedSignedTx = Buffer.from(signedTx.serialize({ requireAllSignatures: false })).toString('base64')
     this.world.network.send('withdrawResponse', { withdrawId, serializedSignedTx })
     // console.log('withdrawResponse', { withdrawId, serializedSignedTx })
